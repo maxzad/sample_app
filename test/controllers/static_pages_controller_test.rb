8300868d9759ac288f46a80867012e4d6dc273a6
Max Zadeh <max.zadeh@gpmec.com>
2017-12-31 22:46:28 -0800

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "NurseLinked is the solution to staffing"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | NurseLinked is the solution to staffing"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | NurseLinked is the solution to staffing"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | NurseLinked is the solution to staffing"
   end
 
 end
